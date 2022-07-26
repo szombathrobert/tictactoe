d0edafe046649b141c480daa78defbc8325edd10
szombathrobert <szombathrobert4@gmail.com>
2022-07-26 11:14:47 +0200

    css és game & board html szerkesztése

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-board',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit {
-
+  @Input() value: 'X'|'O'|undefined
   constructor() { }
 
   ngOnInit(): void {
